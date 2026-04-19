6f1cc54353f61d7c5bb0f007b917c1897c287386
ha_yone <ha_yone@icloud.com>
2026-04-19 19:02:23 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollElements = document.querySelectorAll('.scroll-anim');
+
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend);
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add('visible');
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.1)) {
+                displayScrollElement(el);
+            }
+        })
+    }
+
+    // Trigger on load
+    handleScrollAnimation();
+
+    // Trigger on scroll
+    window.addEventListener('scroll', () => {
+        handleScrollAnimation();
+    });
+});
